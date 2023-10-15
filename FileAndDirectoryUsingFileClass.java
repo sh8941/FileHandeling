a43d0d1f0e8f924765065291c0921a96d2379e98
sh8941 <zshamoonhaider@gmail.com>
2023-10-15 19:25:27 +0530

    File class

--- FileAndDirectoryUsingFileClass.java
+++ FileAndDirectoryUsingFileClass.java
@@ -18,9 +18,35 @@ public class FileAndDirectoryUsingFileClass{
         System.out.println("file status : "+file1.exists()); //true after created
 
         // to create a directory
-        File dir = new File("MyDirectory");
+        File dir = new File("directory");
+        try  {
+            dir.mkdir();
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.getStackTrace();
+        }
         System.out.println("directory status : "+dir.exists());
         dir.mkdirs();
         System.out.println("directory status : "+dir.exists());
+        
+        // to create a file in a directory
+        try {
+            File txt2 = new File(dir, "sampleFile.txt");
+            txt2.createNewFile();
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.getStackTrace();
+        }
+
+        // to print data of a directory
+        File folder = new File("New folder");
+        String[] data = folder.list();
+        int items = 0;
+        System.out.println("New folder data : ");
+        for (String str : data) {
+            System.out.println(str);
+            items++;
+        }
+        System.out.println("Total files : "+items);
     }
 }
